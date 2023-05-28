231569faa81ab995e47852ebb0c97d9df9cbe510
Sebastian Sala <sebastian_orko@hotmail.com>
2023-05-27 19:18:54 -0600

    delete authors && update books added

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -89,7 +89,16 @@ exports.author_create_post = [
 
 exports.author_delete_get = async (req, res, next) => {
   try {
-    res.send("NOT IMPLEMENTED: Author delete POST");
+    const [author, allBooksByAuthor] = await Promise.all([
+      Author.findById(req.params.id).exec(),
+      Book.find({ author: req.params.id }, "title summary").exec(),
+    ]);
+    if (author === null) res.redirect("/catalog/authors");
+    res.render("author_delete", {
+      title: "Delete Author",
+      author: author,
+      author_books: allBooksByAuthor,
+    });
   } catch (e) {
     next(e);
   }
@@ -97,7 +106,22 @@ exports.author_delete_get = async (req, res, next) => {
 
 exports.author_delete_post = async (req, res, next) => {
   try {
-    res.send("NOT IMPLEMENTED: Author delete POST");
+    const [author, allBooksByAuthor] = await Promise.all([
+      Author.findById(req.params.id).exec(),
+      Book.find({ author: req.params.id }, "title summary").exec(),
+    ]);
+
+    if (allBooksByAuthor.length > 0) {
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: author,
+        author_books: allBooksByAuthor,
+      });
+      return;
+    } else {
+      await Author.findByIdAndRemove(req.body.authorid);
+      res.redirect("/catalog/authors");
+    }
   } catch (e) {
     next(e);
   }
